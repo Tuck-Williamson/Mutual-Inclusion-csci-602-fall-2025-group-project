97d0aa6fbc7924a7c375d5c8e96ad5a2f7ba98cc
Dan Johnson <jdjohnson8883@gmail.com>
2025-10-31 19:58:14 -0400

    Adding some small suggestions

--- src/main/java/edu/citadel/api/ListController.java
+++ src/main/java/edu/citadel/api/ListController.java
@@ -2,10 +2,13 @@ package edu.citadel.api;
 
 import edu.citadel.dal.ListEntityRepository;
 import edu.citadel.dal.model.ListEntity;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/list")
@@ -29,19 +32,11 @@ public class ListController {
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ListEntity> deleteList(@PathVariable Long id) {
-        ListEntity list = this.repository.findById(id).orElse(null);
-        if (list != null) {
-            // This is to make sure that the list can be serialized to JSON.
-            // The list items collection is lazily loaded, so there to populate the list we query the size.
-            // Otherwise, because the list is deleted, the data object can't lazy load the list items when serialization occurs.
-            //noinspection ResultOfMethodCallIgnored
-            list.getListItems().size();
-
-            this.repository.delete(list);
+        return repository.findById(id).map(list -> {
+            Hibernate.initialize(list.getListItems()); // initialize the list
+            repository.delete(list);
             return ResponseEntity.ok(list);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        }).orElse(ResponseEntity.notFound().build());
     }
 
 }
