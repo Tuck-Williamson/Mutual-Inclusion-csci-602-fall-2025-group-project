7d5669b7aac816e83b50a981b2c143443f20127d
Tuck Williamson <williamsonth@g.cofc.edu>
2025-10-31 18:16:27 -0400

    Delete endpoint created and working.

--- src/main/java/edu/citadel/api/ListController.java
+++ src/main/java/edu/citadel/api/ListController.java
@@ -5,9 +5,7 @@ import edu.citadel.dal.model.ListEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/list")
@@ -27,6 +25,23 @@ public class ListController {
         ListEntity list = new ListEntity();
 
         return ResponseEntity.ok(repository.save(list));
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ListEntity> deleteList(@PathVariable Long id) {
+        ListEntity list = this.repository.findById(id).orElse(null);
+        if (list != null) {
+            // This is to make sure that the list can be serialized to JSON.
+            // The list items collection is lazily loaded, so there to populate the list we query the size.
+            // Otherwise, because the list is deleted, the data object can't lazy load the list items when serialization occurs.
+            //noinspection ResultOfMethodCallIgnored
+            list.getListItems().size();
+
+            this.repository.delete(list);
+            return ResponseEntity.ok(list);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
