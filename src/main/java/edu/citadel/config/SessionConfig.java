fc01ee970b1e7f82fd2a5b9d898ecfcd63cc6514
Dan Johnson <jdjohnson8883@gmail.com>
2025-11-26 22:39:58 -0500

    reverted sessioning with jdbc (for now...)

--- src/main/java/edu/citadel/config/SessionConfig.java
+++
@@ -1,9 +0,0 @@
-package edu.citadel.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-
-@Configuration
-@EnableJdbcHttpSession
-public class SessionConfig {
-}
